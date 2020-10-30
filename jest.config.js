a5274e2a89b6738315bab955f0bb73a356e61342
Laura <laurazenc@gmail.com>
2020-10-30 16:28:06 +0100

    chore(actions): tweak

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    collectCoverageFrom: ["tests/**/*.{ts,tsx,js,jsx}"],
+    collectCoverageFrom: ["**/*.test.{ts,tsx,js,jsx}"],
     transform: { ".(ts|tsx)$": "ts-jest/dist" },
     transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
     setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
