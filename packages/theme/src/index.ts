9c13ab57e49c18a3626e4928aa00c3f2ecf1a2b7
Laura <laurazenc@gmail.com>
2020-11-01 10:01:31 +0100

    feature(Theme): theme setup

---
+++ packages/theme/src/index.ts
@@ -0,0 +1,7 @@
+const theme = {
+  base: {}, // include global configs box-shadows, sizes, border-radius, ...
+  components: {}, // config for components
+}
+
+export type Theme = typeof theme
+export default theme
