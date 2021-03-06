a7d3d869165c33840c77804a6b186d9c9b29bae0
Laura <laurazenc@gmail.com>
2021-03-06 12:17:58 +0100

    test: update pipeline

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     transform: { ".(ts|tsx)$": "ts-jest/dist" },
     transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
     setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+     coveragePathIgnorePatterns: ["<roorDir>/node_modules/", "<rootDir>/src/index.ts"],
     globals: {
       "ts-jest": {
         tsconfig: "tsconfig.json",
