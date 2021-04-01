5e70c36cbf81e239cfc799880bb5352bf6df1eb9
Laura <laurazenc@gmail.com>
2021-04-01 18:59:28 +0200

    fix: testing

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -3,4 +3,9 @@ const commonConfig = require("../../jest.config")
 
 module.exports = {
   ...commonConfig,
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "<rootDir>/src/atoms/index.ts",
+    "<rootDir>/src/index.ts",
+  ],
 }
